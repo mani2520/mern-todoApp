70e51b1678f038438771752501c489e370f458ee
Manii1997 <manibct1817@gmail.com>
2025-10-02 19:52:22 +0530

    otp issue fixing

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mern-todoapp-ht44.onrender.com/api",
+  baseURL: "https://mern-todoapp-ht44.onrender.com/api", //https://mern-todoapp-ht44.onrender.com/api //http://localhost:5000/
 });
 
 export default api;
