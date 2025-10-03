e4fcc282d82094948c93e8ba5f4750a26deebd3d
Manii1997 <manibct1817@gmail.com>
2025-10-03 12:03:06 +0530

    live server otp issue fixed

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -4,7 +4,9 @@ const sendEmail = async (to, subject, text) => {
   try {
     const transporter = nodemailer.createTransport({
       service: "gmail",
-
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
